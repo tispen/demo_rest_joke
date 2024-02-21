419c2519dcba32c71249aca9d7fe0c2931fc8e1d
julliet <khodyrevajulya@yandex.ru>
2024-02-22 00:54:17 +0300

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		String url = "https://official-joke-api.appspot.com/random_joke";
+
+		WebClient.Builder builder = WebClient.builder();
+
+		String jokeRandom = builder.build()
+				.get()
+				.uri(url)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+		System.out.println("-------------------------");
+		System.out.println(jokeRandom);
+		System.out.println("-------------------------");
+	}
+}
